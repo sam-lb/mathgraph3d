d0caa833c41694806f4522ee200d8c4508e4cdc4
Sam Brunacini <samlbrunacini@gmail.com>
2020-11-07 16:09:05 -0500

    Cleaned up from old version

--- src/Errors.py
+++
@@ -1,2 +0,0 @@
-class GrapherError(Exception):
-    pass;
--- src/Point.py
+++
@@ -1,9 +0,0 @@
-class Point:
-
-    """ A 3D point """
-    
-    def __init__(self, plot, point, color):
-        plot.add_point(self);
-        self.plot = plot;
-        self.point = point;
-        self.color = color;
